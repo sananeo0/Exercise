d8a4f2f257f22a13bb2261627f173550df32fa11
sananeo0 <sananeo@code.edu.az>
2023-08-30 15:48:25 +0400

    Add project files.

---
+++ ArrayHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class ArrayHelper
+    {
+        public static string[] Add(string[] array,string newValue)
+        {
+            Array.Resize(ref array, array.Length+1);
+            array[array.Length-1] = newValue;
+            return array;    
+        }
+        public static string[] Remove(string[] array, string newValue)
+        {
+            int remove = Array.IndexOf(array, newValue);
+            if(remove >= 0)
+            {
+                for (int i = remove; i < array.Length - 1; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+                Array.Resize(ref array, array.Length - 1);
+            }
+            return array;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = new string[] { "a", "b", "c" };
+
+            string[] newArray1 = ArrayHelper.Add(arr, "d");
+            foreach (string item in newArray1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            string[] newArray2 = ArrayHelper.Remove(newArray1, "c");
+            foreach (string item in newArray2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
